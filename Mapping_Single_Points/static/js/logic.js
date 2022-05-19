0eeae7465c9e098f153b422c208c334bb5f6369e
Armstrong <Scott.Armstrong@viasat.com>
2022-05-19 11:00:45 -0700

    initial commit

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,11 +2,23 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+
+// //  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//  Add a circle to the map for Los Angeles, California.
+L.circle([34.0522, -118.2437], {
+    color: 'black',
+    fillColor: 'lightyellow',
+    radius: 300
+ }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: "dark-v10",
+    // id: "light-v10",
     maxZoom: 18,
     accessToken: API_KEY
 });
